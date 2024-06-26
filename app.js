62697c05bc80df4c65c2bdfbd85f31f42e221e83
Dhruv Sharma <sharmadhruv1303@gmail.com>
2024-06-26 22:39:31 +0530

    appointement route

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import doctorsRouter from './routes/getDoctor.js'; 
 import patientsRouter from './routes/storePatient.js'
+import doctorAppointement from "./routes/appointement.js"
 const app=express()
 app.use(cors({
-    origin:"*",
-    Credential:true
+    origin:"http://localhost:3000",
+    credentials:true
 }))
 app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit :"20kb"}))
@@ -17,6 +18,8 @@ app.use(cookieParser())
 app.use('/api/getDoctors', doctorsRouter);
 app.use('/api/storepatients', patientsRouter);
 app.use('/api/storeDoctor', patientsRouter);
+app.use('/api/appoint', doctorAppointement);
+
 
 
 
