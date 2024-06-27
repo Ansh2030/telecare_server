767daf695403d8bf755a79f09ffd452b3471c534
Ansh2030 <anshsrivastava360@gmail.com>
2024-06-27 13:10:42 +0530

    RTC connected and appointment updated

--- routes/appointement.js
+++ routes/appointement.js
@@ -12,31 +12,44 @@ router.post('/bookAppointment', async (req, res)=>{
 
 
 router.post("/update",async(req,res)=>{
-    const{patemail,doctemail,link}=req.body;
-    Appointment.updateOne({
-        patemail:patemail,
-        docemail:doctemail
-    },  {$set: { link: link } }
-    )
+    try{
+        const{patemail,docemail,link}=req.body;
+        console.log(link);
+        await Appointment.updateMany({$and: [
+            { patemail: patemail },
+            { docemail: docemail }
+          ]},  {$set: { link: link } }
+        )
+        res.send("data updated")
+    
+    }
+    catch(e){
+        res.send(e.message);
+    }
 })
+   
 // POST endpoint to save a new patient
 router.post('/getappointment', async (req, res) => {
     try {
-        console.log("hi");
-       const {user, loggedin}=req.body;
-       
-    //    const user= await  Patient.findOne({name})
-       if(!loggedin){
-         return res.status(201).json({
-            message:"User not Found"
-        })
-    }
-        else{
-            // res.redirect("https://chatgpt.com")
-            return res.status(200).json
+        // console.log("hi");
+       const t=req.body;
+    //    console.log(t.user);
+       const allAppointment= await  Appointment.find( {$or: [
+        { patemail: t.user },
+        { docemail: t.user }
+      ]});
+       res.send(allAppointment);
+    //    if(!loggedin){
+    //      return res.status(201).json({
+    //         message:"User not Found"
+    //     })
+    // }
+    //     else{
+    //         // res.redirect("https://chatgpt.com")
+    //         return res.status(200).json
                 
-               ( {message:"user exist"})
-        }
+    //            ( {message:"user exist"})
+    //     }
        
     } catch (err) {
         
